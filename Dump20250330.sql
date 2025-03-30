b860387e65600c70b49b3b45f9c65ebcc78213ec
Quan <petcute2004@gmail.com>
2025-03-30 20:40:13 +0700

    a

--- Dump20250330.sql
+++ Dump20250330.sql
@@ -3,7 +3,7 @@
 -- Host: 127.0.0.1    Database: quanly_mypham
 -- ------------------------------------------------------
 -- Server version	8.0.41
-
+use quanly_mypham;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
